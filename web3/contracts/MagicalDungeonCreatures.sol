20bf8afa7028bc9f73fedc48ecedb4826445a7fe
nevmstas <nevmyvakastas@gmail.com>
2023-02-03 18:45:07 +0600

    player token generation

--- web3/contracts/MagicalDungeonCreatures.sol
+++ web3/contracts/MagicalDungeonCreatures.sol
@@ -6,6 +6,9 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract MagicalDungeonCreatures is ERC1155 {
   string public baseURI;
+  uint256 public totalSupply; // Total number of tokens minted
+
+  uint256 public constant MAX_ATTACK_DEFEND_STRENGTH = 10;
 
   /// @dev Player struct to store player info
   struct Player {
@@ -16,11 +19,22 @@ contract MagicalDungeonCreatures is ERC1155 {
     bool inBattle; /// @param inBattle boolean to indicate if a player is in battle
   }
 
+  /// @dev GameToken struct to store player token info
+  struct GameToken {
+    string name; /// @param name battle card name; set by player
+    uint256 id; /// @param id battle card token id; will be randomly generated
+    uint256 attackStrength; /// @param attackStrength battle card attack; generated randomly
+    uint256 defenseStrength; /// @param defenseStrength battle card defense; generated randomly
+  }
+
   Player[] public players; // Array of players
+  GameToken[] public gameTokens; // Array of game tokens
 
   mapping(address => uint256) public playerInfo;
+  mapping(address => uint256) public playerTokenInfo; // Mapping of player addresses to player token index in the gameTokens array
 
   event NewPlayer(address indexed owner, string name);
+  event NewGameToken(address indexed owner, uint256 id, uint256 attackStrength, uint256 defenseStrength);
 
   /// @dev Initializes the contract by setting a `metadataURI` to the token collection
   /// @param _metadataURI baseURI where token metadata is stored
@@ -35,18 +49,73 @@ contract MagicalDungeonCreatures is ERC1155 {
       return true;
     }
   }
+
+  function getPlayer(address addr) public view returns (Player memory) {
+    require(isPlayer(addr), "Player doesn't exist!");
+    return players[playerInfo[addr]];
+  }
   
   /// @dev Registers a player
   /// @param _name player name; set by player
-  function registerPlayer(string memory _name) external {
+  function registerPlayer(string memory _name, string memory _gameTokenName) external {
     require(!isPlayer(msg.sender), "Player already registered"); // Require that player is not already registered
     
     uint256 _id = players.length;
     players.push(Player(msg.sender, _name, 10, 25, false)); // Adds player to players array
     playerInfo[msg.sender] = _id; // Creates player info mapping
 
-    //TODO: Csreate game token
+    createRandomGameToken(_gameTokenName);
     
     emit NewPlayer(msg.sender, _name); // Emits NewPlayer event
+  }
+
+  /// @dev internal function to generate random number; used for Battle Card Attack and Defense Strength
+  function _createRandomNum(uint256 _max, address _sender) internal view returns (uint256 randomValue) {
+    uint256 randomNum = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, _sender)));
+
+    randomValue = randomNum % _max;
+    if(randomValue == 0) {
+      randomValue = _max / 2;
+    }
+
+    return randomValue;
+  }
+
+  /// @dev internal function to create a new Battle Card
+  function _createGameToken(string memory _name) internal returns (GameToken memory) {
+    uint256 randAttackStrength = _createRandomNum(MAX_ATTACK_DEFEND_STRENGTH, msg.sender);
+    uint256 randDefenseStrength = MAX_ATTACK_DEFEND_STRENGTH - randAttackStrength;
+    
+    uint8 randId = uint8(uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 100);
+    randId = randId % 6;
+    if (randId == 0) {
+      randId++;
+    }
+    
+    GameToken memory newGameToken = GameToken(
+      _name,
+      randId,
+      randAttackStrength,
+      randDefenseStrength
+    );
+
+    uint256 _id = gameTokens.length;
+    gameTokens.push(newGameToken);
+    playerTokenInfo[msg.sender] = _id;
+
+    _mint(msg.sender, randId, 1, '0x0');
+    totalSupply++;
+    
+    emit NewGameToken(msg.sender, randId, randAttackStrength, randDefenseStrength);
+    return newGameToken;
+  }
+
+  /// @dev Creates a new game token
+  /// @param _name game token name; set by player
+  function createRandomGameToken(string memory _name) public {
+    require(!getPlayer(msg.sender).inBattle, "Player is in a battle"); // Require that player is not already in a battle
+    require(isPlayer(msg.sender), "Please Register Player First"); // Require that the player is registered
+    
+    _createGameToken(_name); // Creates game token
   }
 }
